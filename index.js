83d0d6b15a6f49a39a26555d24242e819cc7803c
sakiko21”$ git config --global user.email sakko21f@gmail.com <sakko.21f@gmail.com>
2023-06-15 11:31:49 +0900

    postman使用前

--- index.js
+++ index.js
@@ -4,7 +4,14 @@ import express from "express";
 const app = express();
 const PORT = 3000;
 
-app.get ("/", (req, res) => res.send("Hello World!"));
+app.get ("/", (req, res) => {
+    res.send("<h1>Hello World!</h1>");
+});
+
+app.get ("/users",(req, res) => {
+    res.send("<h1>Hello Users!</h1>");
+    });
+
 app.listen(PORT, ()=>{
     console.log(`Server running on port ${PORT}`);
 });
