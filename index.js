d5f273cf9108d51dd7da9f76200be35e704a5b46
sakiko21”$ git config --global user.email sakko21f@gmail.com <sakko.21f@gmail.com>
2023-06-19 10:15:02 +0900

    ユーザー管理、商品管理を作成

--- index.js
+++ index.js
@@ -12,18 +12,88 @@ const app = express();
 const PORT = 3000;
 //これするとボディにデータ入る
 app.use (express.json());
-
 app.get ("/", (req, res) => {
     res.send("<h1>Hello World!</h1>");
 });
 
-app.post ("/login",(req, res) => {
-    const body = req.body;
-    console.log(body);
-    res.send(body);
-    });
+// app.post ("/login",(req, res) => {
+//     const body = req.body;
+//     console.log(body);
+//     res.send(body);
+// });
+// ユーザー情報のAPI
+app.post ("/user/register", async (req, res) => {
+    const {name, email, password} = req.body;
+    console.log({name, email, password});
+    //会員登録。（会員登録終わってからレスポンスを返したいので、上にasyncで非同期に宣言）
+    const user = await TeckGeekDB.createUser(name, email, password);
+    if (user.error){
+        return res.status(500).send(user.error);
+    }
+    return res.status(200).send(user);
+});
 
-app.listen(PORT, ()=>{
+app.post("/login", async (req, res) => {
+    const {email} = req.body;
+    console.log({email});
+    const user = await TeckGeekDB.getUser(email);
+    console.log({user});
+    if (user.message){
+        return res.status(200).send(user.message);
+    }
+    else if (user.error){
+        return res.status(500).send(user.error);
+    }
+    return res.status(200).send(user);
+});
+
+
+
+//商品情報の登録のAPI
+app.post("/product/create", async (req, res) => {
+    const {title, description, price, imagePath} = req.body;
+    console.log({title, description, price, imagePath});
+    const product = await TeckGeekDB.createProduct(
+        title, 
+        description, 
+        price, 
+        imagePath
+        );
+        console.log({product});
+    if (product.error){
+        console.log(product.error);
+        return res.status(500).send(product.error);
+    }
+    return res.status(200).send(product);
+});
+
+app.get("/products", async (req, res) => {
+    const products = await TeckGeekDB.getProducts();
+    console.log({products});
+    if (products.error){
+        return res.status(500).send(products.error);
+    } else{
+    return res.status(200).send(products);
+    }
+});
+
+app.get("/product/:id", async (req, res) => {
+    const {id} = req.params;
+    console.log({id});
+    const {query_id, query_test} = req.query;
+    console.log({query_id, query_test});
+    const product = await TeckGeekDB.getProduct(id);
+    console.log({product});
+    if (product.message){
+        return res.status(200).send(product.message);
+    } else if (product.error){
+        return res.status(500).send(product.error);
+    }
+    return res.status(200).send(product);
+});
+
+
+app.listen (PORT, ()=>{
     console.log(`Server running on port ${PORT}`);
 });
 
--- teckgeek-db.js
+++ teckgeek-db.js
@@ -99,22 +99,38 @@ export const TeckGeekDB = {
         },
 
             createUser: async (name, email, password) =>{
-                //コネクトを呼び出し、どのデータベースに保存するか設定する
-                const client = await TeckGeekDB.connect();
-                const result = await client.query(
-                    `INSERT INTO users (name, email, password) VALUES ($1, $2, $3) RETURNING *;`,
-                    [name, email, password]
-                );
-                return result.rows[0];
-            },
+                //エラー時の処理
+                try {
+                    //コネクトを呼び出し、どのデータベースに保存するか設定する
+                    const client = await TeckGeekDB.connect();
+                    const user = await TeckGeekDB.getUser(email);
+                    if(user){
+                        return {error: 'このメールアドレスは登録されています'};
+                    } else {
+                        const result = await client.query(
+                            `INSERT INTO users (name, email, password) VALUES ($1, $2, $3) RETURNING *;`,
+                            [name, email, password]
+                        );
+                        return result.rows[0];
+                    }
+                } catch(error){
+                    console.log(error);
+                    return {error: '不明なエラーが発生しました'}
+                }
+                },
             //ユーザーの情報を取得する
             getUser: async (email) => {
+                try{
                 const client = await TeckGeekDB.connect();
                 const result = await client.query(
                     `SELECT * FROM users WHERE email = $1;`,
                     [email]
                 );
-                return result.rows[0];
+                return result.rows[0] || {message:'ユーザーが見つかりません'};
+            } catch(error){
+                console.log(error);
+                return {error: '不明なエラーが発生しました'}
+            }
             },
             //IDでも取得できるように
             getUserById: async (id) => {
@@ -126,30 +142,45 @@ export const TeckGeekDB = {
                 return result.rows[0];
             },
             //商品を作成する
-            createProduct: async (title, description, price, imagePath) => {
+            createProduct: async (title, description, price, image_path) => {
+                try {
                 const client = await TeckGeekDB.connect();
                 const result = await client.query(
                     `INSERT INTO products (title, description, price, image_path) VALUES ($1, $2, $3, $4) RETURNING *;`,
-                    [title, description, price, imagePath]
+                    [title, description, price, image_path]
                 );
                 return result.rows[0];
+            } catch(error){
+                console.log(error);
+                return {error: '不明なエラーが発生しました'}
+            }
             },
             //全ての商品情報をとってくる
             getProducts: async () => {
+                try{
                 const client = await TeckGeekDB.connect();
                 const result = await client.query(
                     `SELECT * FROM products;`
                 );
                 return result.rows;
+            } catch(error){
+                console.log(error);
+                return {error: '不明なエラーが発生しました'}
+            }
             },
             //商品IDから商品情報を取得する（商品詳細ページで特定の商品情報をとってくる必要がある）
-            getProductById: async (id) => {
+            getProduct: async (id) => {
+                try{
                 const client = await TeckGeekDB.connect();
                 const result = await client.query(
                     `SELECT * FROM products WHERE id = $1;`,
                     [id]
                 );
-                return result.rows[0];
+                return result.rows[0] || {message:'商品が見つかりません'};
+            } catch(error){
+                console.log(error);
+                return {error: '不明なエラーが発生しました'}
+            }
             },
             createPurchase: async (user_id, amount, product_ids) => {
                 const client = await TeckGeekDB.connect();
@@ -166,5 +197,23 @@ export const TeckGeekDB = {
             [user_id]
         );
         return result.rows;
+    },
+    //会員情報を更新する
+    User: async (id, name, email, password) => {
+        const client = await TeckGeekDB.connect();
+        const result = await client.query(
+            `UPDATE users SET name = $1, email = $2, password = $3 WHERE id = $4 RETURNING *;`,
+            [name, email, password, id]
+        );
+        return result.rows[0];
+    },
+    //商品を更新する
+    updateProduct: async (id, title, description, price, imagePath) => {
+        const client = await TeckGeekDB.connect();
+        const result = await client.query(
+            `UPDATE products SET title = $1, description = $2, price = $3, image_path = $4 WHERE id = $5 RETURNING *;`,
+            [title, description, price, imagePath, id]
+        );
+        return result.rows[0];
     }
 }
